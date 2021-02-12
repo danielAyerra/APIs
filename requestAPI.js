9506e232c3395124711440b1122ec9fbff31eea0
Weinman <danyerra@gmail.com>
2021-02-12 12:50:34 +0100

    Commit 1202211250 	new file:   movies.html 	new file:   requestAPI.js 	Created html and js files. Ongoing. 	HTML file has some basic text and HTML5 tags 	requestAPI.js has basic backbone and is slightly commented. LAME

---
+++ requestAPI.js
@@ -0,0 +1,26 @@
+/*
+	requestAPI.js
+	Manages REST requests and returns information to webpage
+	Simplest API evaa!!!!
+	Functions: Post, get, put, delete.
+	Comments will be improved for using a proper, standard, code comments. Need to refresh that on google.
+*/
+//PostMethod -> Uses post request for getting information
+postMethod(){
+
+}
+
+//GetMethod -> Uses get request for getting information
+getMethod(){
+
+}
+
+//PutMethod -> Uses put request for sending information
+putMethod(){
+
+}
+
+//DeleteMethod -> Uses delete request for deleting information
+deleteMethod(){
+
+}
