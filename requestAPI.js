f80166241d180e67896b0f1111b6165d715b8a03
Weinman <danyerra@gmail.com>
2021-02-17 10:42:45 +0100

    Commit 1702211043 - Fully Express server (Basic Functions) Completed Express server and request API. Front pending 	modified:   requestAPI.js 	modified:   routes.js

--- requestAPI.js
+++ requestAPI.js
@@ -10,22 +10,6 @@
 const port = "3000";
 const server = "http://localhost";
 
-//PostMethod -> Uses post request for getting information
-function postMethod(reqType, newObj, id) {
-	if (!reqType||!newObj){
-		console.error("Unable to do the request");
-	} else {
-		console.log(reqType, newObj);
-		const req=server+':'+port+'/'+reqType+'/'+id;
-		fetch(req,{mode:'no-cors', method:'POST', headers:{'Content-type':'application/json'}, body: JSON.stringify(newObj)})
-			.then(response=>{
-				console.log(response);
-			})
-			.catch(err=>{console.error(err);});
-	}
-}
-
-
 //GetMethod -> Uses get request for getting information
 function getMethod(reqType){
 	let req;
@@ -40,7 +24,7 @@ function getMethod(reqType){
 		if(response.ok===true){
 			response.json().then(res=>{
 				return res;
-			});
+			}).catch(err=>console.error(err));
 		} else{
 			console.error('Error. Status code: '+ response.status + ' '+ response.statusText);
 		}
@@ -49,36 +33,81 @@ function getMethod(reqType){
 }
 
 //PutMethod -> Uses put request for sending information
-function putMethod(){
-	let req="PUT "+server+":"+port;
+function putMethod(reqType, newObj, id){
+	if (!reqType||!newObj){
+		console.error("Unable to do the request");
+	} else {
+		const req=server+':'+port+'/'+reqType+'/'+id;
+		fetch(req,
+			{	mode:'cors', 
+				method:'PUT', 
+				headers:
+					{'Content-Type':'application/json'
+				}, 
+				body: JSON.stringify(newObj)
+			}
+
+			).then(response=>{
+				if(response.ok===true){
+					response.json().then(res=>{
+						return res;
+					}).catch(err=>console.error(err));
+				} else{
+					console.error('Error. Status code: '+ response.status + ' '+ response.statusText);
+				}
+			})
+			.catch(err=>console.error(err));
+	}
+}
+
+//PostMethod -> Uses post request for getting information
+function postMethod(reqType, newObj, id){
+	if (!reqType||!newObj){
+		console.error("Unable to do the request");
+	} else {
+		const req=server+':'+port+'/'+reqType+'/'+id;
+		fetch(req,
+			{	mode:'cors', 
+				method:'POST', 
+				headers:
+					{'Content-Type':'application/json'
+				}, 
+				body: JSON.stringify(newObj)
+			}
+
+		).then(response=>
+			{
+				if(response.ok===true){
+					response.json().then(res=>{
+						return res;
+					}).catch(err=>console.error(err));
+				} else{
+					console.error('Error. Status code: '+ response.status + ' '+ response.statusText);
+				}
+			}
+		).catch(err=>{console.error(err);});
+	}
 }
 
 //DeleteMethod -> Uses delete request for deleting information
-function deleteMethod(){
-	let req="DELETE "+server+":"+port;
+function deleteMethod(reqType, id){
+	if (!reqType||!id){
+		console.error("Unable to do the request");
+	} else {
+		const req=server+':'+port+'/'+reqType+'/'+id;
+		fetch(req,
+			{	
+				mode:'cors', 
+				method:'DELETE', 
+			}
+		).then(response=>{
+			if(response.ok===true){
+				response.json().then(res=>{
+					return res;
+				}).catch(err=>console.error(err));
+			} else{
+				console.error('Error. Status code: '+ response.status + ' '+ response.statusText);
+			}
+		})
+		.catch(err=>{console.error(err);});
 }
-
-/* Uso de FETCH
-// Ejemplo implementando el metodo POST:
-async function postData(url = '', data = {}) {
-  // Opciones por defecto estan marcadas con un *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-postData('https://example.com/answer', { answer: 42 })
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });*/
--- routes.js
+++ routes.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const mockData = JSON.parse(fs.readFileSync('mockData.json'));
 
 
+
 /* GET movies listing. */
 router.get('/:type', function(req, res, next) {
 	const reqType = req.params.type;
 	if (mockData.hasOwnProperty(reqType)){
-		console.log(mockData[reqType]);
-		res.status(200).send(mockData[reqType]);
+		res.status(200).json(mockData[reqType]);
 	}
 	else{
 		console.log("Not recognised");
@@ -17,20 +17,45 @@ router.get('/:type', function(req, res, next) {
 	}
 });
 
-router.get('/', function(req, res, next){
+router.get('/', function(req, res, next) {
 	res.status(200).send(mockData);
 });
 
-router.post('/:type/:id', function (req, res, next){
+router.post('/:type/:id', function (req, res, next) {
 	const reqType = req.params.type;
-	console.log(reqType);
 	const id = req.params.id;
-	console.log(req.body);
-	console.log(id);
+	const rawData = req.body;
 	if(mockData.hasOwnProperty(reqType)){
-		const rawData = req.body;
-		console.log(rawData);
-		res.status(200).send("ok");
+		mockData[reqType].push(rawData);
+		res.status(200).json({message: "Added Succesfully"});	}
+	else{
+		res.status(400).json({message:"Unrecognized request"});
+	}
+});
+
+router.put('/:type/:id', function (req, res, next) {
+	const reqType = req.params.type;
+	const id = req.params.id;
+	const rawData = req.body;
+	if(mockData.hasOwnProperty(reqType)){
+		mockData[reqType].push(rawData);
+		res.status(200).json({message: "Added Succesfully"});
+	}
+	else{
+		res.status(400).json({message:"Unrecognized request"});
+	}
+});
+
+router.delete('/:type/:id', function (req, res, next) {
+	const reqType = req.params.type;
+	const id = req.params.id;
+	if(mockData.hasOwnProperty(reqType)){
+		if(mockData[reqType].hasOwnProperty(id)){
+			res.status(200).json({message: "Deleted Succesfully"});
+		}
+		else{
+			res.status(404).json({message: "Not found resource"});
+		}
 	}
 	else{
 		res.status(400).json({message:"Unrecognized request"});
