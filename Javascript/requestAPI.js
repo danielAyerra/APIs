d0ece52e3d329823a1228dc94d6decb72284ccf7
Weinman <danyerra@gmail.com>
2021-02-26 08:42:08 +0100

    Commit 2602210842 fix: Just quit some spare comment lines

--- Javascript/requestAPI.js
+++ Javascript/requestAPI.js
@@ -5,7 +5,6 @@
 	Functions: Post, get, put, delete.
 	Comments will be improved for using a proper, standard, code comments. Need to refresh that on google.
 */
-//PostMethod -> Uses post request for getting information
 
 const port = "3000";
 const server = "http://localhost";
@@ -23,31 +22,6 @@ const getMethod = async function(reqType){
 
 	return response.json();
 }
-//GetMethod -> Uses get request for getting information
-/*const getMethod = function(reqType){
-	let req;
-	if(!reqType){
-		req=server+":"+port;
-	}
-	else{
-		req=`${server}:${port}/${reqType}`;
-	}
-	fetch(req,{mode:'cors'})
-	.then(response=>{
-		if(response.ok===true){
-			response.json().then(res=>{
-				console.log(res);
-				return res;
-			}).catch(err=>console.error(err));
-		} else{
-			console.error('Error. Status code: '+ response.status + ' '+ response.statusText);
-			return {message: 'Failed'};
-		}
-	})
-	.catch(err=>{console.error(err);
-		return {message: 'Failed'}
-	});
-}*/
 
 //PutMethod -> Uses put request for sending information
 const putMethod = function (reqType, newObj, id){
