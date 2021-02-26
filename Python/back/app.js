1a6f3208f99a9031240daf10bab33359714d25d2
Weinman <danyerra@gmail.com>
2021-02-26 08:30:19 +0100

    Commit 2602210830 feature: HTML page with CSS style Improved style for proper view TODO: - Arrange info into cards 	  - Make it responsive 	  - Make @media for smaller screens 	  - Fix side images (why the central image is displayed wrong??) 	  - Add format to admin features 	  - Give functionality to admin features

---
+++ Python/back/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = require('./routes');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/',router);
+
+app.listen(3000, ()=> console.log("Server listens"));
+module.exports = app;
